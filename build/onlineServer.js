639d6b29848f79625271af273c6532c3e68f4f5f
dangyuan <892848278@qq.com>
2018-01-29 23:51:55 +0800

    优化首页路径

--- build/onlineServer.js
+++ build/onlineServer.js
@@ -10,8 +10,8 @@ let port = process.env.PORT || 3002;
 let path = require('path');
 let app = express();
 //  使用静态资源
-app.use('/static', express.static('static'));
-app.use('/react', express.static('react'));
+app.use('/static', express.static('./static'));
+app.use('/react', express.static('./react'));
 //代理请求服务器图片，接口跨域问题
 app.use('/imgPro[0-9]', function(req, res) {
     let url = 'https://img'+req.baseUrl.split("/imgPro")[1]+'.doubanio.com' + req.url;
